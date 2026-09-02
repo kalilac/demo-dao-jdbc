c10b25bd520b2b6ce6a4c2900698ce5e2d4ebed0
Kalil de A. Carvalho <kalilac@gmail.com>
2026-09-02 15:44:48 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,6 +30,13 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		System.out.println("\n=== TEST 3: seller findAll ====");
+		list = sellerdao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+
 
 
 
